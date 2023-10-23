d014e0224c162ad906b1ac213c6fd18f2b94ed23
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2023-10-23 16:39:28 +0100

    Ending of the basics explanation of react with the setting up of the development environment.

---
+++ React18WithMosh/react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+//There is normally two ways to create react components
+/*we can use javascript classcomponents or functions (which is the must used)*/
+
+//pascal casing
+function Message(){
+    //JSX: Javascript XML
+    const name = 'christophe';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message;
