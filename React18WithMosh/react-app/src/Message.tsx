497f4c99f604c8ee40e732cc611bd4747caa59d2
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2023-10-23 19:09:48 +0100

    Starting of the concep of creating components, use fragments and conditional rendering.

--- React18WithMosh/react-app/src/Message.tsx
+++ React18WithMosh/react-app/src/Message.tsx
@@ -7,7 +7,7 @@ function Message(){
     const name = 'christophe';
     if (name)
         return <h1>Hello {name}</h1>
-    return <h1>Hello world</h1>
+    return <h1>Hello world</h1> 
 }
 
 export default Message;
