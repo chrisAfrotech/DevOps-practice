cf870fa8d6a18a35f2c1f6aeaf0a23373722c123
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2023-10-26 18:38:20 +0100

    Manipulation of the text and using of the numberOfLine for truncate and onpress to get text clicked log.

--- ReactNativeWithMosh/DoneWithIt/App.js
+++ ReactNativeWithMosh/DoneWithIt/App.js
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function App() {
-  let x = 1;
-  console.log("App executed");
-  
+export default function App() {  
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text> Done with our app</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}> 
+        Hello react native. A really long text. Now i want to make this even longer and see what happens for very very long text. 
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
   },
 });
