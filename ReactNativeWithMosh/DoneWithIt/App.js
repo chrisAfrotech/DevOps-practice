5d5e7d1f2ed27d6fdc882bd47749a9069f058ae1
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2023-10-27 17:05:39 +0100

    Manipulation of button component done

--- ReactNativeWithMosh/DoneWithIt/App.js
+++ ReactNativeWithMosh/DoneWithIt/App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TouchableHighlight, TouchableNativeFeedback, Image, SafeAreaView } from 'react-native';
+import { StyleSheet,SafeAreaView, Button } from 'react-native';
 
 export default function App() {  
   const handlePress = () => console.log("Text pressed");
   console.log(require("./assets/splash.png"));
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}> 
-        Hello react native. A really long text. Now i want to make this even longer and see what happens for very very long text. 
-      </Text>
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
-        <View style={{width:200, height:70 ,backgroundColor:"dodgerblue"}}></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button color ="orange" title="click Me" onPress={() => console.log("Button clicked")} />
     </SafeAreaView>
   );
 }
