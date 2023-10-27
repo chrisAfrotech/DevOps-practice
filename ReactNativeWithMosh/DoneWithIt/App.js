feca62854993ef816b00cb1255872342221f3bae
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2023-10-27 16:56:03 +0100

    Manipulation of the images with touchable effect done.

--- ReactNativeWithMosh/DoneWithIt/App.js
+++ ReactNativeWithMosh/DoneWithIt/App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,TouchableHighlight, TouchableNativeFeedback, Image, SafeAreaView } from 'react-native';
 
 export default function App() {  
   const handlePress = () => console.log("Text pressed");
+  console.log(require("./assets/splash.png"));
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}> 
         Hello react native. A really long text. Now i want to make this even longer and see what happens for very very long text. 
       </Text>
+      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}>
+        <View style={{width:200, height:70 ,backgroundColor:"dodgerblue"}}></View>
+      </TouchableNativeFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -16,6 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
